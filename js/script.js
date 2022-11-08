60dc6e90bc6746ccb933d657ff0721e2229cd4eb
Avers1o <atlant1696@mail.ru>
2022-11-08 11:59:02 +0300

    Added axios

--- js/script.js
+++ js/script.js
@@ -218,36 +218,17 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-    getResourses('http://localhost:3000/menu')
-    .then(data => {
-        data.forEach(({img, altimg, title, descr, price}) => {
-            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-        });
-    });
-
     // getResourses('http://localhost:3000/menu')
-    // .then(data => createCard(data));
-
-    // function createCard(data) {
+    // .then(data => {
     //     data.forEach(({img, altimg, title, descr, price}) => {
-    //         const element = document.createElement('div');
-
-    //         element.classList.add('menu__item');
-
-    //         element.innerHTML = `
-    //             <img src=${img} alt=${altimg}>
-    //             <h3 class="menu__item-subtitle">${title}</h3>
-    //             <div class="menu__item-descr">${descr}</div>
-    //             <div class="menu__item-divider"></div>
-    //             <div class="menu__item-price">
-    //                 <div class="menu__item-cost">Цена:</div>
-    //                 <div class="menu__item-total"><span>${price * 62}</span> руб/день</div>
-    //             </div>
-    //         `;
-
-    //         document.querySelector('.menu .container').append(element);
+    //         new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
     //     });
-    // }
+    // });
+
+    axios.get('http://localhost:3000/menu')
+    .then(data => data.data.forEach(({img, altimg, title, descr, price}) => {
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    }));
 
 
 
