b5d443c181c79925738c61760da772819b42da19
Avers1o <atlant1696@mail.ru>
2022-11-11 01:43:04 +0300

    Added slider dots

--- js/script.js
+++ js/script.js
@@ -219,11 +219,17 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     getResourses('http://localhost:3000/menu')
-    .then(data => {
-        data.forEach(({img, altimg, title, descr, price}) => {
-            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+        .then(data => {
+            data.forEach(({
+                img,
+                altimg,
+                title,
+                descr,
+                price
+            }) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
         });
-    });
 
     // axios.get('http://localhost:3000/menu')
     //     .then(data => data.data.forEach(({img, altimg, title, descr, price}) => {
@@ -322,6 +328,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // Усложненный вариант.
 
     const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -336,7 +343,7 @@ window.addEventListener('DOMContentLoaded', () => {
     modifiedCounter(total, slides.length);
     modifiedCounter(current, slideIndex);
 
-    slidesField.style.width = 100 * slides.length +'%';
+    slidesField.style.width = 100 * slides.length + '%';
     slidesField.style.display = 'flex';
     slidesField.style.transition = '0.5s all';
 
@@ -345,6 +352,26 @@ window.addEventListener('DOMContentLoaded', () => {
     slides.forEach(slide => {
         slide.style.width = width;
     });
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+        dots = [];
+    indicators.classList.add('carousel-indicators');
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.classList.add('dot');
+        dot.setAttribute('data-slide-to', i + 1);
+
+        if (i === 0) {
+            dot.style.opacity = 1;
+        }
+
+        indicators.append(dot);
+        dots.push(dot);
+    }
 
     next.addEventListener('click', () => {
         if (offset === parseInt(width) * (slides.length - 1)) {
@@ -358,10 +385,12 @@ window.addEventListener('DOMContentLoaded', () => {
         if (slideIndex === slides.length) {
             slideIndex = 1;
         } else {
-            slideIndex ++;
+            slideIndex++;
         }
 
         modifiedCounter(current, slideIndex);
+
+        activateDot();
     });
 
     prev.addEventListener('click', () => {
@@ -378,8 +407,23 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             slideIndex--;
         }
-
+        
         modifiedCounter(current, slideIndex);
+
+        activateDot();
+    });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (event) => {
+            slideIndex = +event.target.getAttribute('data-slide-to');
+            offset = parseInt(width) * (slideIndex - 1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            modifiedCounter(current, slideIndex);
+
+            activateDot();
+        });
     });
 
     function modifiedCounter(counter, value) {
@@ -388,5 +432,10 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             counter.textContent = value;
         }
+    }
+
+    function activateDot () {
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = '1';
     }
 });
